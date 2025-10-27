268e740435f007039bc402c01f2de15bf6898fce
Gadgets-X online store <renetajean90@gmail.com>
2025-10-27 05:09:38 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+function buyNow(itemName, amount) {
+  FlutterwaveCheckout({
+    public_key: "FLWPUBK-2b01ca9653cbf025d8843ce27e456c4b-X",
+    tx_ref: "RaztuneFLW_" + Date.now(),
+    amount: amount,
+    currency: "NGN",
+    payment_options: "card,banktransfer,ussd",
+    customer: {
+      email: "customer@example.com",
+      phone_number: "08100000000",
+      name: itemName,
+    },
+    customizations: {
+      title: "Raztune FLW",
+      description: "Payment for " + itemName,
+      logo: "images/logo.jpg",
+    },
+    callback: function (data) {
+      // Redirect after payment success
+      window.location.href = "https://form.svhrt.com/68ee7fa9c526e9f3b64609a9";
+    },
+    onclose: function () {
+      alert("Payment window closed.");
+    },
+  });
+}
